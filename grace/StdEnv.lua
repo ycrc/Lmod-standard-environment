d23daaf10b8608505a16285b0fd893faab6ca7fd
Kaylea Nelson <kaylea.nelson@yale.edu>
2018-06-06 15:56:00 -0400

    add reference to admin.list

--- grace/StdEnv.lua
+++ grace/StdEnv.lua
@@ -9,7 +9,10 @@ local moduledirs = {
 for i = #moduledirs, 1, -1 do
     prepend_path("MODULEPATH", pathJoin(rootpath, moduledirs[i]))
 end
---
 
+
+-- Add module tracking, addtl paths
 setenv ("LMOD_PACKAGE_PATH", "/gpfs/loomis/apps/hpc.rhel7")
 prepend_path("PATH", "/apps/bin")
+-- File for deprecation messages
+setenv ("LMOD_ADMIN_FILE", pathJoin("/gpfs/loomis/apps/hpc.rhel7","admin.list"))
