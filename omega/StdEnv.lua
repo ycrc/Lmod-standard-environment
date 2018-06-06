d88bc0483da020af2c8099b7670e427247409813
Kaylea Nelson <kaylea.nelson@yale.edu>
2018-06-06 16:09:03 -0400

    omega: add reference to admin.list

--- omega/StdEnv.lua
+++ omega/StdEnv.lua
@@ -9,7 +9,9 @@ local moduledirs = {
 for i = #moduledirs, 1, -1 do
     prepend_path("MODULEPATH", pathJoin(rootpath, moduledirs[i]))
 end
---
 
+-- Add module tracking, addtl paths
 setenv ("LMOD_PACKAGE_PATH", "/apps/hpc.rhel7")
 prepend_path("PATH", "/apps/bin")
+-- File for deprecation messages
+setenv ("LMOD_ADMIN_FILE", pathJoin("/apps/hpc.rhel7","admin.list"))
