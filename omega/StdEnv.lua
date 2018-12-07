d42814a1ca1d7547a0f2105abae82d7658343b82
Kaylea Nelson <kaylea.nelson@yale.edu>
2018-12-07 09:39:48 -0500

    add loomis project environment variable to Omega

--- omega/StdEnv.lua
+++ omega/StdEnv.lua
@@ -1,21 +1,23 @@
 add_property("lmod", "sticky")
 --
-local rootpath = "/apps/hpc.rhel7"
+local rootpath = "/gpfs/loomis/home.omega/apps/hpc.rhel7"
 local moduledirs = {
     "Modules",
     "Modules.rhel6"
 }
 
-local home = os.getenv("HOME")
-local scratch = home:gsub("/home", "/gpfs/loomis/scratch60")
-setenv ("SCRATCH", scratch)
-
 for i = #moduledirs, 1, -1 do
     prepend_path("MODULEPATH", pathJoin(rootpath, moduledirs[i]))
 end
 
+local home = os.getenv("HOME")
+local scratch = home:gsub("/home", "/gpfs/loomis/scratch60")
+setenv ("SCRATCH", scratch)
+local project = home:gsub("/home", "/gpfs/loomis/project")
+setenv ("LOOMIS_PROJECT", project)
+
 -- Add module tracking, addtl paths
-setenv ("LMOD_PACKAGE_PATH", "/apps/hpc.rhel7")
+setenv ("LMOD_PACKAGE_PATH", "/gpfs/loomis/home.omega/apps/hpc.rhel7")
 prepend_path("PATH", "/apps/bin")
 -- File for deprecation messages
-setenv ("LMOD_ADMIN_FILE", pathJoin("/apps/hpc.rhel7","admin.list"))
+setenv ("LMOD_ADMIN_FILE", pathJoin("/gpfs/loomis/home.omega/apps/hpc.rhel7","admin.list"))
