dbe6afced0eae4778ff9f74a998276d440fe17f0
Kaylea Nelson <kaylea.nelson@yale.edu>
2018-12-07 09:38:42 -0500

    added project and scratch environment variables to Grace

--- grace/StdEnv.lua
+++ grace/StdEnv.lua
@@ -10,6 +10,11 @@ for i = #moduledirs, 1, -1 do
     prepend_path("MODULEPATH", pathJoin(rootpath, moduledirs[i]))
 end
 
+local home = os.getenv("HOME")
+local scratch = home:gsub("/home", "/gpfs/loomis/scratch60")
+setenv ("LOOMIS_SCRATCH", scratch)
+local project = home:gsub("/home", "/gpfs/loomis/project")
+setenv ("LOOMIS_PROJECT", project)
 
 -- Add module tracking, addtl paths
 setenv ("LMOD_PACKAGE_PATH", "/gpfs/loomis/apps/hpc.rhel7")
