4c8eb1b9ae76e75a42599ca10f96650c2c4dcdff
Kaylea Nelson <kaylea.nelson@yale.edu>
2018-12-11 13:33:22 -0500

    add milgram

---
+++ milgram/StdEnv.lua
@@ -0,0 +1,23 @@
+add_property("lmod", "sticky")
+-- Set module path
+local rootpath = "/gpfs/milgram/apps/hpc.rhel7/modules"
+local moduledirs = {
+    "base", "bio", "cae", "chem", "compiler", "data", "debugger", "devel",
+    "geo", "ide", "lang", "lib", "math", "mpi", "numlib", "perf", "phys",
+    "system", "toolchain", "tools", "vis"
+}
+
+for i = #moduledirs, 1, -1 do
+    prepend_path("MODULEPATH", pathJoin(rootpath, moduledirs[i]))
+end
+
+-- Add module tracking, addtl paths
+setenv ("LMOD_PACKAGE_PATH", "/gpfs/milgram/apps")
+prepend_path("PATH", "/gpfs/milgram/apps/bin")
+
+-- File for depreciation messages
+setenv ("LMOD_ADMIN_FILE", pathJoin(rootpath,"admin.list"))
+
+-- Use module cache for loads
+setenv ("LMOD_CACHED_LOAD", "yes")
+
