35095b49d7accd46d1e53af910590b5dc3e91225
Kaylea Nelson <kaylea.nelson@yale.edu>
2018-12-14 15:45:55 -0500

    Milgram: add .ycrc to StdEnv

--- milgram/StdEnv.lua
+++ milgram/StdEnv.lua
@@ -11,6 +11,10 @@ for i = #moduledirs, 1, -1 do
     prepend_path("MODULEPATH", pathJoin(rootpath, moduledirs[i]))
 end
 
+-- Standard environment variables
+prepend_path("PATH", pathJoin(os.getenv("HOME"),".ycrc/bin"))
+prepend_path("LD_LIBRARY_PATH", pathJoin(os.getenv("HOME"),".ycrc/lib"))
+
 -- Add module tracking, addtl paths
 setenv ("LMOD_PACKAGE_PATH", "/gpfs/milgram/apps")
 prepend_path("PATH", "/gpfs/milgram/apps/bin")
