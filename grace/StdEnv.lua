cf96bf84e1e985543f78ac366bfe8c345451a41a
Kaylea Nelson <kaylea.nelson@yale.edu>
2019-03-07 11:23:01 -0500

    add slurm hint and conda env

--- grace/StdEnv.lua
+++ grace/StdEnv.lua
@@ -11,13 +11,22 @@ for i = #moduledirs, 1, -1 do
 end
 
 local home = os.getenv("HOME")
-local scratch = home:gsub("/home", "/gpfs/loomis/scratch60")
-setenv ("LOOMIS_SCRATCH", scratch)
-local project = home:gsub("/home", "/gpfs/loomis/project")
-setenv ("LOOMIS_PROJECT", project)
+if ( home ~= nil ) then
+    scratch = home:gsub("/home", "/gpfs/loomis/scratch60")
+    setenv ("LOOMIS_SCRATCH", scratch)
+    project = home:gsub("/home", "/gpfs/loomis/project")
+    setenv ("LOOMIS_PROJECT", project)
+end
+
+-- For Hyperthreading protection
+setenv ("SLURM_HINT", "nomultithread")
 
 -- Add module tracking, addtl paths
 setenv ("LMOD_PACKAGE_PATH", "/gpfs/loomis/apps/hpc.rhel7")
-prepend_path("PATH", "/apps/bin")
+prepend_path("PATH", "/gpfs/loomis/apps/bin")
+
 -- File for deprecation messages
 setenv ("LMOD_ADMIN_FILE", pathJoin("/gpfs/loomis/apps/hpc.rhel7","admin.list"))
+
+-- Set default conda install directory to be in project
+prepend_path("CONDA_ENVS_PATH", pathJoin(project, "conda_envs"))
