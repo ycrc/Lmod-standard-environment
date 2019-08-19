2215492d161e3bdab01844dd02dc4130e0a3b5f9
Kaylea Nelson <kaylea.nelson@yale.edu>
2019-08-19 11:51:07 -0400

    updated Grace StdEnv for module update, etc

--- grace/StdEnv.lua
+++ grace/StdEnv.lua
@@ -1,15 +1,17 @@
 add_property("lmod", "sticky")
 --
-local rootpath = "/apps"
+local rootpath = "/gpfs/loomis/apps/avx/modules"
 local moduledirs = {
-    "hpc.rhel7/Modules",
-    "hpc.rhel6/Modules"
+    "base", "bio", "cae", "chem", "compiler", "data", "debugger", "devel",
+    "geo", "ide", "lang", "lib", "math", "mpi", "numlib", "perf", "phys",
+    "system", "toolchain", "tools", "vis", "restricted"
 }
 
 for i = #moduledirs, 1, -1 do
     prepend_path("MODULEPATH", pathJoin(rootpath, moduledirs[i]))
 end
 
+-- Standard Environment Variables
 local home = os.getenv("HOME")
 if ( home ~= nil ) then
     scratch = home:gsub("/home", "/gpfs/loomis/scratch60")
@@ -17,12 +19,13 @@ if ( home ~= nil ) then
     project = home:gsub("/home", "/gpfs/loomis/project")
     setenv ("LOOMIS_PROJECT", project)
 end
+setenv ("SQUEUE_FORMAT", "%18i %11P %18j %4u %.2t %.10M %.10l %.5D %.5C %.10m %R")
 
 -- For Hyperthreading protection
 setenv ("SLURM_HINT", "nomultithread")
 
 -- Add module tracking, addtl paths
-setenv ("LMOD_PACKAGE_PATH", "/gpfs/loomis/apps/hpc.rhel7")
+setenv ("LMOD_PACKAGE_PATH", "/gpfs/loomis/apps/")
 prepend_path("PATH", "/gpfs/loomis/apps/bin")
 
 -- File for deprecation messages
