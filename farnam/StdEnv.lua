cc1280282f9c6bc118f93acb6c012cc5abb4db44
Ping Luo of group hpcprog <pl543@grace2.grace.hpc.yale.internal>
2019-10-18 17:03:33 -0400

    add groupname in scratch and project

--- farnam/StdEnv.lua
+++ farnam/StdEnv.lua
@@ -19,8 +19,8 @@ for g in grps:split("[ \n]") do
     defaultGroup = g
     break
 end
-local scratch = pathJoin(common_prefix, "/scratch60", os.getenv("USER"))
-local project = pathJoin(common_prefix, "/project",   os.getenv("USER"))
+local scratch = pathJoin(common_prefix, "/scratch60", defaultGroup, os.getenv("USER"))
+local project = pathJoin(common_prefix, "/project",   defaultGroup, os.getenv("USER"))
 setenv ("YSM_SCRATCH", scratch)
 setenv ("YSM_PROJECT", project)
 
