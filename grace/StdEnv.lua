7e004dadb71f15a26a4668a0f1a36acf17eb3024
Kaylea Nelson <kaylea.nelson@yale.edu>
2022-04-14 11:46:16 -0400

    add LMOD_SHORT_TIME to grace

--- grace/StdEnv.lua
+++ grace/StdEnv.lua
@@ -23,8 +23,10 @@ for g in grps:split("[ \n]") do
 end
 local scratch = pathJoin(common_prefix, "/scratch60", defaultGroup, os.getenv("USER"))
 local project = pathJoin(common_prefix, "/project",   defaultGroup, os.getenv("USER"))
+local palmer_scratch = pathJoin("/vast/palmer/scratch", defaultGroup, os.getenv("USER"))
 setenv ("LOOMIS_SCRATCH", scratch)
 setenv ("LOOMIS_PROJECT", project)
+setenv ("PALMER_SCRATCH", palmer_scratch)
 
 -- Standard Environment Variables
 setenv ("SQUEUE_FORMAT", "%18i %11P %18j %6u %.2t %.10M %.10l %.5D %.5C %.10m %R")
@@ -32,16 +34,21 @@ setenv ("SQUEUE_FORMAT", "%18i %11P %18j %6u %.2t %.10M %.10l %.5D %.5C %.10m %R
 -- For Hyperthreading protection
 setenv ("SLURM_HINT", "nomultithread")
 
+-- Extended History Formatting --
+setenv ("HISTTIMEFORMAT", "%Y-%m-%d %T ")
+
 -----
 -- Lmod Options
 -----
 
+setenv ("LMOD_SHORT_TIME", 86400)
+
 -- Add module tracking, addtl paths
-setenv      ("LMOD_PACKAGE_PATH", pathJoin(common_prefix, "/apps"))
+setenv      ("LMOD_PACKAGE_PATH", "/vast/palmer/apps/avx.grace")
 prepend_path("PATH",              pathJoin(common_prefix, "/bin" ))
 
 -- File for deprecation messages
-setenv ("LMOD_ADMIN_FILE", pathJoin(common_prefix, "/apps/hpc.rhel7", "admin.list"))
+setenv ("LMOD_ADMIN_FILE", pathJoin("/vast/palmer/apps/avx.grace", "admin.list"))
 
 -- Make module searches sort case insensitive
 setenv ("LMOD_CASE_INDEPENDENT_SORTING", "yes")
